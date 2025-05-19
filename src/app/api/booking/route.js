7474d28a315ddb58bf38ce667e527be8a8cec481
OneGit2200 <sjpetersona@gmail.com>
2025-05-19 10:33:09 +0300

    .

--- src/app/api/booking/route.js
+++ src/app/api/booking/route.js
@@ -26,7 +26,7 @@ export async function POST(request) {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_SERVER_HOST,
       port: parseInt(process.env.EMAIL_SERVER_PORT),
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.EMAIL_SERVER_USER,
         pass: process.env.EMAIL_SERVER_PASSWORD,
